d86b5c5b06b0d0dc1b6b34bfebdfdaf3ceb17421
Andrés Proaño <srvhubprod@usfq.edu.ec>
2026-02-07 18:55:52 -0500

    new logic program

---
+++ front-personal/src/views/Home.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="menu-conteiner">
+        <h1>House Organizer</h1>
+        <div class="grid-menu">
+            <route-link to="/kitchen-storage" class="menu-card kitchen">
+                <span class="icon"></span>
+                <h2>Kitchen Storage</h2>
+                <p>Manage your kitchen inventory and storage efficiently.</p>
+            </route-link>
+
+            <route-link to="/finances" class="menu-card finances">
+                <span class="icon"></span>
+                <h2>Finances</h2>
+                <p>Track your expenses and manage your household budget.</p>
+            </route-link>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.menu-conteiner {
+    display: flex;
+    text-align: center;
+    flex-direction: column;
+    align-items: center;
+}
+
+.grid-menu{
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 20px;
+    margin-top: 40px;
+}
+
+@media (min-width: 768px) {
+  .grid-menu { grid-template-columns: 1fr 1fr; } /* Dos columnas en PC */
+}
+
+.menu-card {
+  padding: 30px;
+  border-radius: 15px;
+  text-decoration: none;
+  color: white;
+  transition: transform 0.2s;
+  box-shadow: 0 4px 10px rgba(0,0,0,0.1);
+}
+
+.menu-card:active { transform: scale(0.95); }
+
+.kitchen { background-color: #4caf50; }
+.money { background-color: #2196f3; }
+
+.icon { font-size: 3rem; }
+</style>
---
+++ front-personal/src/views/loginView.vue
@@ -0,0 +1,52 @@
+<script setup>
+import { onMounted } from 'vue'
+import { supabase } from '../lib/supabaseClient'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const handleGoogleLogin = async (response) => {
+  const { data, error } = await supabase.auth.signInWithIdToken({
+    provider: 'google',
+    token: response.credential,
+  })
+
+  if (error) {
+    console.error('Error al entrar:', error.message)
+  } else {
+    console.log('¡Bienvenido!', data.user)
+    router.push('/dashboard')
+  }
+}
+
+onMounted(() => {
+  google.accounts.id.initialize({
+    client_id: import.meta.env.VITE_GOOGLE_CLIENT_ID,
+    callback: handleGoogleLogin,
+  })
+
+  google.accounts.id.renderButton(
+    document.getElementById("googleBtn"),
+    { theme: "filled_blue", size: "large", width: "250" }
+  )
+})
+</script>
+
+<template>
+  <div class="flex-container">
+    <h1>Mi Portal Personal</h1>
+    <p>Inicia sesión para gestionar tus finanzas y cocina</p>
+    <div id="googleBtn"></div>
+  </div>
+</template>
+
+<style scoped>
+.flex-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  text-align: center;
+}
+</style>
